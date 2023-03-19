c333d860822f85424192a70c1727d2b881d1fcda
Raducu <raducu.pescaru@itperspectives.ro>
2023-03-19 07:41:06 +0200

    Add project files.

---
+++ ProiectAStarAI/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ProiectAStarAI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textX = new TextBox();
+            textY = new TextBox();
+            textBox3 = new TextBox();
+            textXF = new TextBox();
+            textYF = new TextBox();
+            btnFindPath = new Button();
+            testText = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Menu;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(222, 12);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.ShortcutsEnabled = false;
+            textBox1.Size = new Size(354, 26);
+            textBox1.TabIndex = 0;
+            textBox1.TabStop = false;
+            textBox1.Text = "Problema navigarii robotului spre casa";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Menu;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Arial Narrow", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(21, 79);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.ShortcutsEnabled = false;
+            textBox2.Size = new Size(98, 15);
+            textBox2.TabIndex = 1;
+            textBox2.TabStop = false;
+            textBox2.Text = "Punctele de plecare:";
+            // 
+            // textX
+            // 
+            textX.BorderStyle = BorderStyle.FixedSingle;
+            textX.Location = new Point(125, 77);
+            textX.Name = "textX";
+            textX.Size = new Size(26, 23);
+            textX.TabIndex = 2;
+            // 
+            // textY
+            // 
+            textY.BorderStyle = BorderStyle.FixedSingle;
+            textY.Location = new Point(157, 77);
+            textY.Name = "textY";
+            textY.Size = new Size(26, 23);
+            textY.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.Menu;
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Font = new Font("Arial Narrow", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(21, 125);
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.ShortcutsEnabled = false;
+            textBox3.Size = new Size(98, 15);
+            textBox3.TabIndex = 4;
+            textBox3.TabStop = false;
+            textBox3.Text = "Punctele de sosire:";
+            // 
+            // textXF
+            // 
+            textXF.BorderStyle = BorderStyle.FixedSingle;
+            textXF.Location = new Point(125, 124);
+            textXF.Name = "textXF";
+            textXF.Size = new Size(26, 23);
+            textXF.TabIndex = 5;
+            // 
+            // textYF
+            // 
+            textYF.BorderStyle = BorderStyle.FixedSingle;
+            textYF.Location = new Point(157, 124);
+            textYF.Name = "textYF";
+            textYF.Size = new Size(26, 23);
+            textYF.TabIndex = 6;
+            // 
+            // btnFindPath
+            // 
+            btnFindPath.Location = new Point(343, 214);
+            btnFindPath.Name = "btnFindPath";
+            btnFindPath.Size = new Size(89, 23);
+            btnFindPath.TabIndex = 8;
+            btnFindPath.Text = "Gaseste calea";
+            btnFindPath.UseVisualStyleBackColor = true;
+            btnFindPath.Click += btnFindPath_Click;
+            // 
+            // testText
+            // 
+            testText.Location = new Point(370, 114);
+            testText.Name = "testText";
+            testText.Size = new Size(100, 23);
+            testText.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(testText);
+            Controls.Add(btnFindPath);
+            Controls.Add(textYF);
+            Controls.Add(textXF);
+            Controls.Add(textBox3);
+            Controls.Add(textY);
+            Controls.Add(textX);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "AStarAI";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textX;
+        private TextBox textY;
+        private TextBox textBox3;
+        private TextBox textXF;
+        private TextBox textYF;
+        private Button btnFindPath;
+        private TextBox testText;
+    }
+}
---
+++ ProiectAStarAI/Form1.cs
@@ -0,0 +1,217 @@
+
+
+using System.Drawing.Design;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ProiectAStarAI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Coordonatele unei celule
+        public class Coordinates : IEquatable<Coordinates>
+        {
+            public int row;
+            public int col;
+
+            public Coordinates() { this.row = -1; this.col = -1; }
+            public Coordinates(int row, int col) { this.row = row; this.col = col; }
+
+            public bool Equals(Coordinates c)
+            {
+                if (this.row == c.row && this.col == c.col)
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        public class Cell
+        {
+            public int cost;
+            public int g;
+            public int f;
+            public Coordinates parent;
+        }
+
+        Cell[,] cells = new Cell[8, 8];
+        List<Coordinates> path = new List<Coordinates>();
+        List<Coordinates> opened = new List<Coordinates>();
+        List<Coordinates> closed = new List<Coordinates>();
+        Coordinates startCell;
+        Coordinates finishCell;
+
+        public void AStar()
+        {
+            var x = int.Parse(textX.Text);
+            var y = int.Parse(textY.Text);
+            var xF = int.Parse(textXF.Text);
+            var yF = int.Parse(textYF.Text);
+
+            startCell = new Coordinates(x, y);
+            finishCell = new Coordinates(xF, yF);
+
+            if (IsWall(x, y) || IsWall(xF, y))
+            {
+                System.Console.WriteLine("Nu exista solutii, alegeti alte coordonate.");
+                return;
+            }
+
+            for (int i = 0; i < 8; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    cells[i, j] = new Cell();
+                    cells[i, j].parent = new Coordinates();
+                    if (IsWall(i, j))
+                        cells[i, j].cost = 100;
+                    else
+                        cells[i, j].cost = 1;
+                }
+
+            opened.Add(startCell);
+
+            Boolean pathFound = false;
+
+            do
+            {
+                List<Coordinates> neighbors = new List<Coordinates>();
+                Coordinates currentCell = ShorterExpectedPath();
+                neighbors = neighborCells(currentCell);
+
+                foreach (Coordinates newCell in neighbors)
+                {
+                    if (newCell.row == finishCell.row && newCell.col == finishCell.col)
+                    {
+                        cells[newCell.row, newCell.col].g = cells[currentCell.row, currentCell.col].g + cells[newCell.row, newCell.col].cost;
+                        cells[newCell.row, newCell.col].parent.row = currentCell.row;
+                        cells[newCell.row, newCell.col].parent.col = currentCell.col;
+                        pathFound = true;
+                        break;
+                    }
+                    else if (!opened.Contains(newCell) && !closed.Contains(newCell))
+                    {
+                        cells[newCell.row, newCell.col].g = cells[currentCell.row, currentCell.col].g + cells[newCell.row, newCell.col].cost;
+                        cells[newCell.row, newCell.col].f = cells[newCell.row, newCell.col].g + Heuristic(newCell);
+                        cells[newCell.row, newCell.col].parent.row = currentCell.row;
+                        cells[newCell.row, newCell.col].parent.col = currentCell.col;
+                        SetCell(newCell, opened);
+                    }
+                    else if (cells[newCell.row, newCell.col].g > cells[currentCell.row, currentCell.col].g + cells[newCell.row, newCell.col].cost)
+                    {
+                        cells[newCell.row, newCell.col].g = cells[currentCell.row, currentCell.col].g + cells[newCell.row, newCell.col].cost;
+                        cells[newCell.row, newCell.col].f = cells[newCell.row, newCell.col].g + Heuristic(newCell);
+                        cells[newCell.row, newCell.col].parent.row = currentCell.row;
+                        cells[newCell.row, newCell.col].parent.col = currentCell.col;
+                        SetCell(newCell, opened);
+                        ResetCell(newCell, closed);
+                    }
+                }
+
+                SetCell(currentCell, closed);
+                ResetCell(currentCell, opened);
+
+            } while (opened.Count > 0 && pathFound == false);
+
+            if (pathFound)
+            {
+                path.Add(finishCell);
+                Coordinates currentCell = new Coordinates(finishCell.row, finishCell.col);
+                while (cells[currentCell.row, currentCell.col].parent.row >= 0)
+                {
+                    path.Add(cells[currentCell.row, currentCell.col].parent);
+                    int tmp_row = cells[currentCell.row, currentCell.col].parent.row;
+                    currentCell.col = cells[currentCell.row, currentCell.col].parent.col;
+                    currentCell.row = tmp_row;
+                }
+
+                //afisarea gridului
+            }
+        }
+
+        public bool IsWall(int row, int col)
+        {
+            int[,] walls = new int[,] { { 1, 5 }, { 2, 2 }, { 4, 0 }, { 5, 5 }, { 6, 6 } };
+            bool isWall = false;
+            for (int i = 0; i < walls.GetLength(0); i++)
+            {
+                if (walls[i, 0] == row && walls[i, 1] == col)
+                    isWall = true;
+            }
+            return isWall;
+        }
+
+        public Coordinates ShorterExpectedPath()
+        {
+            int sep = 0;
+            if (opened.Count > 1)
+            {
+                for (int i = 1; i < opened.Count; i++)
+                {
+                    if (cells[opened[i].row, opened[i].col].f < cells[opened[sep].row, opened[sep].col].f)
+                    {
+                        sep = i;
+                    }
+                }
+            }
+            return opened[sep];
+        }
+
+        public List<Coordinates> neighborCells(Coordinates c)
+        {
+            List<Coordinates> lc = new List<Coordinates>();
+
+            for (int i = -1; i <= 1; i++)
+            {
+                if (c.row + i >= 0 && c.row + i < 8 && c.col + i >= 0 && c.col + i < 8 && (i != 0))
+                {
+                    lc.Add(new Coordinates(c.row + i, c.col));
+                    lc.Add(new Coordinates(c.row, c.col + i));
+                }
+
+                if (c.row + i > 7 && c.col + i <= 7)
+                {
+                    lc.Add(new Coordinates(c.row, c.col + i));
+                }
+
+                if (c.col + i > 7 && c.row + i <= 7)
+                {
+                    lc.Add(new Coordinates(c.row + i, c.col));
+                }
+            }
+            return lc;
+        }
+
+        public int Heuristic(Coordinates cell)
+        {
+            int dRow = Math.Abs(finishCell.row - cell.row);
+            int dCol = Math.Abs(finishCell.col - cell.col);
+            return Math.Max(dRow, dCol);
+        }
+
+        public void SetCell(Coordinates cell, List<Coordinates> coordinatesList)
+        {
+            if (coordinatesList.Contains(cell) == false)
+            {
+                coordinatesList.Add(cell);
+            }
+        }
+
+        public void ResetCell(Coordinates cell, List<Coordinates> coordinatesList)
+        {
+            if (coordinatesList.Contains(cell))
+            {
+                coordinatesList.Remove(cell);
+            }
+        }
+
+        private void btnFindPath_Click(object sender, EventArgs e)
+        {
+            AStar();
+        }
+    }
+}
